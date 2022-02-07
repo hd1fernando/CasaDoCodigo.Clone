b7a492f15c38b96732062b3fe0e6205be7a652d9
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-02-07 20:47:52 -0300

    feat: create project Api #12

---
+++ backend/adpters/CasaDoCodigo.Clone.Api/Controllers/AuthorController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Clone.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthorController: ControllerBase{
+    
+}
