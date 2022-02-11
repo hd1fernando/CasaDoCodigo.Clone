f9505bd175aa46272ad60bacac192e768889cf36
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-02-10 21:41:53 -0300

    feat: configure Email table

---
+++ backend/adpters/CasaDoCodigo.Clone.DatabaseAccess/Mappings/EmailEntityMap.cs
@@ -0,0 +1,15 @@
+using CasaDoCodigo.Clone.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CasaDoCodigo.Clone.DatabaseAccess.Mappings;
+
+public class EmailEntityMap : IEntityTypeConfiguration<Email>
+{
+    public void Configure(EntityTypeBuilder<Email> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Value)
+            .IsRequired();
+    }
+}
