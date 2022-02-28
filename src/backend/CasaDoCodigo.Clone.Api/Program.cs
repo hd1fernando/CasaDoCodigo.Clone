2a5b5c1ef7d77398e1bffa80fea75d4888062245
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-02-28 20:59:38 -0300

    fix: reorganize project folders #12

---
+++ src/backend/CasaDoCodigo.Clone.Api/Program.cs
@@ -0,0 +1,32 @@
+using CasaDoCodigo.Clone.DatabaseAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+// Add services to the container.
+builder.Services.AddDbContext<CasaDoCodigoDbContext>(
+    options => 
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("CasaDoCodigo.Clone.Api")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
