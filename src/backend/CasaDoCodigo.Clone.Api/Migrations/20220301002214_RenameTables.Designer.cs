3f879d1d8c09656c6dc982c66a548f454a6e03bb
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-02-28 21:42:27 -0300

    feat: add migrations to rename table #26

--- src/backend/CasaDoCodigo.Clone.Api/Migrations/20220301002214_RenameTables.Designer.cs
+++ src/backend/CasaDoCodigo.Clone.Api/Migrations/20220301002214_RenameTables.Designer.cs
@@ -4,6 +4,7 @@ using CasaDoCodigo.Clone.DatabaseAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CasaDoCodigo.Clone.Api.Migrations
 {
     [DbContext(typeof(CasaDoCodigoDbContext))]
-    partial class CasaDoCodigoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220301002214_RenameTables")]
+    partial class RenameTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/backend/CasaDoCodigo.Clone.Api/Migrations/20220301002214_RenameTables.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CasaDoCodigo.Clone.Api.Migrations
+{
+    public partial class RenameTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Author_Email_EmailId",
+                table: "Author");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Email",
+                table: "Email");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Author",
+                table: "Author");
+
+            migrationBuilder.RenameTable(
+                name: "Email",
+                newName: "Emails");
+
+            migrationBuilder.RenameTable(
+                name: "Author",
+                newName: "Authors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Author_EmailId",
+                table: "Authors",
+                newName: "IX_Authors_EmailId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Emails",
+                table: "Emails",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Authors_Emails_EmailId",
+                table: "Authors",
+                column: "EmailId",
+                principalTable: "Emails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Authors_Emails_EmailId",
+                table: "Authors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Emails",
+                table: "Emails");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors");
+
+            migrationBuilder.RenameTable(
+                name: "Emails",
+                newName: "Email");
+
+            migrationBuilder.RenameTable(
+                name: "Authors",
+                newName: "Author");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Authors_EmailId",
+                table: "Author",
+                newName: "IX_Author_EmailId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Email",
+                table: "Email",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Author",
+                table: "Author",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Author_Email_EmailId",
+                table: "Author",
+                column: "EmailId",
+                principalTable: "Email",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
