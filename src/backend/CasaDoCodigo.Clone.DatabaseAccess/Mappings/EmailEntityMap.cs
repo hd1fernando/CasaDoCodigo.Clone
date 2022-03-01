fc20ac09c07d6e25623762063825a65a63fec4a1
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-02-28 21:24:40 -0300

    fix: change table names #26

--- src/backend/CasaDoCodigo.Clone.DatabaseAccess/Mappings/EmailEntityMap.cs
+++ src/backend/CasaDoCodigo.Clone.DatabaseAccess/Mappings/EmailEntityMap.cs
@@ -11,5 +11,7 @@ public class EmailEntityMap : IEntityTypeConfiguration<Email>
         builder.HasKey(p => p.Id);
         builder.Property(p => p.Value)
             .IsRequired();
+
+        builder.ToTable("Emails");
     }
 }
