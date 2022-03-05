691fc9b6f0abe2d6244f5f8d2d4991586208fa67
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2022-03-05 18:56:47 -0300

    refactor: move interfaces for an especific folder #12

---
+++ src/backend/CasaDoCodigo.Clone.Domain/Interfaces/Repository/IAuthorRepository.cs
@@ -0,0 +1,9 @@
+﻿using CasaDoCodigo.Clone.Domain.Entities;
+
+namespace CasaDoCodigo.Clone.Domain.Repository;
+
+public interface IAuthorRepository
+{
+    public Task CreateAuthorAsync(AuthorEntity author, CancellationToken cancellationToken);
+    public Task<AuthorEntity> GetAuthorByEmailAsync(Email email, CancellationToken cancellationToken);
+}
---
+++ src/backend/CasaDoCodigo.Clone.Domain/Interfaces/Service/IAuthorService.cs
@@ -0,0 +1,7 @@
+﻿using CasaDoCodigo.Clone.Domain.Entities;
+
+namespace CasaDoCodigo.Clone.Domain.Interfaces.Service;
+public interface IAuthorService
+{
+    public Task CreateAuthorAsync(AuthorEntity author, CancellationToken cancellation);
+}
